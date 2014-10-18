6112aa50b6b99828e6634e2e62e5ac1b6552fb4c
Cat McGuinness <maiamcguinness@gmail.com>
2014-10-18 00:53:35 -0500

    Still waffling on where to put server.js, and in the meanwhile, adding the routing back -- this is now fully functional

---
+++ backend/routes/index.js
@@ -0,0 +1,11 @@
+/**
+ * File: backend/routes/index.js
+ * Description: This file defines the route for the main index.html file
+ * Dependencies: none
+ *
+ * @package gamma
+ */
+
+exports.index = function(req, res) {
+    res.sendfile('app/index.html');
+};
